72608975ab0ae762144996bbce368c6c5aa21ca5
Eric Gallager <egall@gwmail.gwu.edu>
2022-12-28 00:32:58 -0500

    Update metadata.js
    changes based on AramZS/tweetback/seo-smo-metadata-improvements

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
+
 let data = {
 	username: "cooljeanius", // No leading @ here
 	homeLabel: "cooljeanius.github.io",
 	homeUrl: "https://cooljeanius.github.io/tweetback/",
+	baseURL:
+		process.env.ENV == "local"
+			? "http://localhost:8080"
+			: "https://cooljeanius.github.io/my_tweetback_archive",
+	language: "en-US",
 };
 
-data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
+data.avatar = `${data.baseUrl}/img/avatar.jpeg`;
 
 module.exports = data;
+
